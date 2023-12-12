d0a40149a91ac4534519b52914dd232bc1f1d99d
Muhammet Enes AY <enesaym@gmail.com>
2023-12-13 02:47:55 +0300

    login register ekranları tasarimlarına baslandi

---
+++ AdvanceUI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        public AuthController()
+        {
+				
+        }
+        public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+	}
+}
--- AdvanceUI/Startup.cs
+++ AdvanceUI/Startup.cs
@@ -46,7 +46,7 @@ namespace AdvanceUI
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+					pattern: "{controller=Auth}/{action=Login}/{id?}");
 			});
 		}
 	}
