deb54728869d32b6fe9bbd4cb49344522791704f
Muhammet Enes AY <enesaym@gmail.com>
2023-12-13 02:03:40 +0300

    template uygulandi

---
+++ AdvanceUI/Controllers/AdvanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceUI.Controllers
+{
+    public class AdvanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
